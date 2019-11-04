b71a771536af04ee9ec96c578e36a96c382f8c16
lidejian <892379244@qq.com>
2019-11-04 13:34:45 +0800

    哎 表单验证

--- pages/search/add_merchant/add_merchant.vue
+++ pages/search/add_merchant/add_merchant.vue
@@ -2,37 +2,68 @@
 	<view class="full">
 		<form>
 			<view class="cu-form-group margin-top">
-				<view class="title">姓名</view>
-				<input placeholder="被推荐人姓名..." name="input" v-model="formData.name"></input>
+				<view class="title">客商姓名</view>
+				<input placeholder="客商姓名..." name="input" @input="inputChange($event, 'name')" v-model="formData.name"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.name == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.name == false"></text>
 			</view>
-			<view class="cu-form-group">
-				<view class="title">性别</view>
-				<picker @change="PickerChange" :value="index" :range="gender">
-					<view class="picker">
-						{{index>-1?gender[index]:'选择性别...'}}
-					</view>
-				</picker>
+			<view class="cu-form-group margin-top">
+				<view class="title">负责人</view>
+				<input placeholder="负责人..." name="input" @input="inputChange($event, 'manager')" v-model="formData.manager"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.manager== true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="false == dataValidate.manager"></text>
 			</view>
-			<view class="cu-form-group">
-				<view class="title">联系电话</view>
-				<input placeholder="被推荐人联系电话..." name="input" v-model="formData.contactPhone"></input>
+			<view class="cu-form-group margin-top">
+				<view class="title">公司地址</view>
+				<input placeholder="公司地址..." name="input" @input="inputChange($event, 'addr')" v-model="formData.addr"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.addr== true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.addr== false"></text>
 			</view>
-			<view class="cu-form-group">
-				<view class="title">情况介绍</view>
-				<textarea placeholder="被推荐人情况介绍..." name="input" v-model="formData.remark"></textarea>
+			<view class="cu-form-group margin-top">
+				<view class="title">主营业务</view>
+				<input placeholder="主营业务..." name="input" @input="inputChange($event, 'mainBusiness')" v-model="formData.mainBusiness"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.mainBusiness == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.mainBusiness == false"></text>
 			</view>
-			
-			<view class="cu-form-group">
-				<view class="title">适合项目</view>
-				<picker @change="ProjectChange" :value="projectIndex" :range="projects">
-					<view class="picker">
-						{{projectIndex>-1?projects[projectIndex]:'最适合的项目...'}}
-					</view>
-				</picker>
+			<view class="cu-form-group margin-top">
+				<view class="title">投资意向</view>
+				<input placeholder="投资意向..." name="input" @input="inputChange($event, 'intention')" v-model="formData.intention"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.intention == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.intention == false"></text>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">联系人</view>
+				<input placeholder="联系人..." name="input" @input="inputChange($event, 'contacts')" v-model="formData.contacts"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.contacts == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.contacts == false"></text>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">联系人职务</view>
+				<input placeholder="联系人职务..." name="input" @input="inputChange($event, 'contactsPost')" v-model="formData.contactsPost"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.contactsPost == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.contactsPost == false"></text>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">联系人电话</view>
+				<input placeholder="联系人电话..." name="input" @input="inputChange($event, 'contactsPhone')" v-model="formData.contactsPhone"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.contactsPhone == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.contactsPhone == false"></text>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">来源地</view>
+				<input placeholder="来源地..." name="input" @input="inputChange($event, 'source')" v-model="formData.source"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.source == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.source == false"></text>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">商会属性</view>
+				<input placeholder="商会属性..." name="input" @input="inputChange($event, 'attribute')" v-model="formData.attribute"></input>
+				<text class='cuIcon-roundcheckfill text-green' v-if="dataValidate.attribute == true"></text>
+				<text class='cuIcon-roundclosefill text-red' v-if="dataValidate.attribute == false"></text>
 			</view>
 			
 			<view class="padding flex flex-direction">
-				<button class="cu-btn bg-blue lg" @click="submit()" :disabled="isChecked">提交</button>
+				<button class="cu-btn bg-blue lg" @click="submit()" :disabled="!submitAble">提交</button>
 			</view>
 		</form>
 	</view>
@@ -42,12 +73,63 @@
 	export default {
 		data() {
 			return {
+				submitAble: false,
 				formData: {
 					name: '',
-					gender: null,
-					contactPhone: '',
-					remark: '',
-					projectId: null
+					manager: '',
+					addr: '',
+					mainBusiness: '',
+					intention: '',
+					contacts: '',
+					contactsPost: '',
+					contactsPhone: '',
+					source: '',
+					attribute: ''
+				},
+				dataValidate: {
+					name: null,
+					manager: null,
+					addr: null,
+					mainBusiness: null,
+					intention: null,
+					contacts: null,
+					contactsPost: null,
+					contactsPhone: null,
+					source: null,
+					attribute: null
+				},
+				dataValidator: {
+					name: {
+						require: true
+					},
+					manager: {
+						require: true
+					},
+					addr: {
+						require: true
+					},
+					mainBusiness: {
+						require: true
+					},
+					intention: {
+						require: true
+					},
+					contacts: {
+						require: true
+					},
+					contactsPost: {
+						require: true
+					},
+					contactsPhone: {
+						require: true,
+						match: /^1(3|4|5|7|8)\d{9}$/
+					},
+					source: {
+						require: true
+					},
+					attribute: {
+						require: true
+					}
 				},
 				index: -1,
 				projectIndex: -1,
@@ -57,9 +139,41 @@
 			}
 		},
 		methods: {
+			validateInput(inputValue, attribute) {
+				if(this.dataValidator[attribute]) {
+					if(this.dataValidator[attribute].require) {
+						if(inputValue != null && inputValue != '') {
+							this.dataValidate[attribute] = true;
+						}else {
+							this.dataValidate[attribute] = false;
+						}
+					}
+					if(this.dataValidator[attribute].match) {
+						if(inputValue != null && inputValue != '') {
+							if(inputValue.match(this.dataValidator[attribute].match) != null) {
+								this.dataValidate[attribute] = true;
+							}else {
+								this.dataValidate[attribute] = false;
+							}
+						}
+					}
+				}
+			},
+			inputChange(e, attribute) {
+				this.validateInput(e.detail.value, attribute);
+				for(let i in this.dataValidate) {
+					if(this.dataValidate[i] == true){
+						continue;
+					} else {
+						this.submitAble = false;
+						return ;
+					}
+					
+				}
+				this.submitAble = true;
+			},
 			submit() {
-				
-				this.$http.post('oa/zsTalenteder', this.formData).then(resp => {
+				this.$http.post('oa/zsMerchant', this.formData).then(resp => {
 					uni.navigateBack({});
 				});
 			},
@@ -96,14 +210,6 @@
 			this.loadProjects();
 		},
 		computed: {
-			isChecked() {
-				for (let s in this.formData) {
-					if(this.formData[s] === null || this.formData[s] === '') {
-						return true;
-					}
-				}
-				return false;
-			}
 		}
 	}
 </script>
